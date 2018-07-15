f5d947996114283ec354dc58dc346be342b28e8d
Armand Rozanski <armand@rozanski.pro>
2018-07-15 13:26:40 +0100

    add "about" section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import style from "./main.scss";
 
+const a = document.getElementsByTagName('a');
+
+Object.keys(a).forEach(function(obj) {
+  a[obj].addEventListener('click', function(event) {
+    event.preventDefault();
+  }, false);
+});
+
 console.log('Hello from Armand');
