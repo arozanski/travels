2f0c4314d8750773bdf09b2a1ac4d40e4567f55d
Armand Rozanski <armand@rozanski.pro>
2018-07-16 17:31:45 +0100

    add css popup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import style from "./main.scss";
 
-const a = document.getElementsByTagName('a');
-
-Object.keys(a).forEach(function(obj) {
-  a[obj].addEventListener('click', function(event) {
-    event.preventDefault();
-  }, false);
-});
-
 console.log('Hello from Armand');
